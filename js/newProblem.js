75ad14e26c639a7baa2e530d3ec08552db91ae2b
wuyuekang <754496162@qq.com>
2018-01-18 01:47:11 +0800

    完成五种问题的新建页面

--- js/newProblem.js
+++ js/newProblem.js
@@ -4,44 +4,79 @@ newProblem.controller('newProblemCtrl',function ($scope, APIService) {
     $scope.initData = function () {
 
         //判断题参数init
-        $scope.initJudge();
-
-        //选择题参数
-        $scope.initChoose();
+        $scope.initJudgement();
 
 
     }
 
-    $scope.initJudge = function () {
+    $scope.initJudgement = function () {
         $scope.problemTypes = constent.problemTypes;
         $scope.problemType = constent.problemTypes[0];
         $scope.judge = 'yes'
     }
 
-    $scope.initChoose = function () {
+    $scope.initChoice = function () {
         $scope.optionAmounts = [1,2,3,4,5,6];
         $scope.optionAmount = 4;
         $scope.optionArray = 'ABCDEFG'.slice(0,4).split('');
         $scope.optionContent = [];
+        $scope.rightOption = 'A'
+    }
+    
+    $scope.initFilling = function () {
+        $scope.blankAmounts = [1,2,3,4,5,6];
+        $scope.blankAmount = 1;
+        $scope.blankArray = $scope.blankAmounts.slice(0,1);
+        $scope.codeContent = [];
+        $scope.blankContent = [];
+    }
+
+    $scope.initCorrecting = function () {
+        $scope.errorAmounts = [1,2,3,4,5,6];
+        $scope.errorAmount = 1;
+        $scope.errorArray = $scope.errorAmounts.slice(0,1);
+        $scope.codeContent = [];
+        $scope.errorContent = [];
+        $scope.rightContent = [];
+    }
+
+    $scope.initProgramming = function () {
+        $scope.inputDes = "";
+        $scope.outputDes = "";
     }
 
     $scope.changeModel = function (type) {
+        $scope.problemForm.submitted = false;
         if(type.no == 1){
-            $scope.initJudge();
+            $scope.initJudgement();
         }else if(type.no == 2){
-            $scope.initChoose();
+            $scope.initChoice();
+        }else if(type.no == 3){
+            $scope.initFilling();
+        }else if(type.no == 4){
+            $scope.initCorrecting();
+        }else if(type.no == 5){
+            $scope.initProgramming();
         }
     }
 
-    $scope.updateArray = function(i){
+    $scope.updateOptionArray = function(i){
         $scope.optionArray = 'ABCDEFG'.slice(0,i).split('');
     }
-    
+
+    $scope.updateBlankArray = function (i) {
+        $scope.blankArray = $scope.blankAmounts.slice(0,i);
+    }
+
+    $scope.updateErrorArray = function (i) {
+        $scope.errorArray = $scope.errorAmounts.slice(0,i);
+    }
+
     $scope.submit = function () {
-        console.log($scope.optionContent['B'])
+        // console.log($scope.optionContent['B'])
         console.log($scope.problemForm)
-        console.log($scope.problemForm.optionAmount.$invalid)
-        console.log($scope.problemForm['optionContent[A]'].$invalid)
+        // console.log($scope.problemForm.optionAmount.$invalid)
+        // console.log($scope.problemForm['optionContent[A]'].$invalid)
         $scope.problemForm.submitted = true;
         if(!$scope.problemForm.$valid){
             layer.msg('请输入必填项！')
